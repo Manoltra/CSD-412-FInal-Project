09f5cca09a131dbdae50def0c689d9b873939089
Traian Manolescu <tpm@micro-workflow.com>
2025-12-04 17:50:28 -0800

    redid the database model

--- models/BudgetChart.js
+++ models/BudgetChart.js
@@ -2,6 +2,11 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../database');
 
 const BudgetChart = sequelize.define('BudgetChart', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
   userId: {
     type: DataTypes.INTEGER,
     allowNull: false,
@@ -13,6 +18,23 @@ const BudgetChart = sequelize.define('BudgetChart', {
   amount: {
     type: DataTypes.FLOAT,
     allowNull: false,
+  },
+  budget: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  expense: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cost: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
   },
 }, {
   timestamps: true,
