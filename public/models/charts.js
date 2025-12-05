6343f0bef47b79e04bca912f3b67b33c80f62fde
Traian Manolescu <tpm@micro-workflow.com>
2025-12-04 16:28:08 -0800

    Postgresql is implemented afaik- admin username and password are listed in config.json for testing purposes for the time being

--- public/models/charts.js
+++ public/models/charts.js
@@ -1,12 +1,17 @@
 const ctx = document.getElementById('myChart').getContext('2d');
 const chart = new Chart(ctx, {
-  type: 'line',
+  type: 'doughnut',
   data: {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgb(255, 99, 132)',
+      label: 'Expenses',
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 205, 86)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+      ],
       data: [0, 10, 5, 2, 20, 30, 45]
     }]
   },
